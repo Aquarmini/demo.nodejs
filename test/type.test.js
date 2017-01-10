16b8db0a132e0bec174943ae7569fc46971bec18
limx-ubuntu <715557344@qq.com>
2017-01-10 19:26:55 +0800

    add type.test.js

---
+++ test/type.test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by limx on 17-1-10.
+ */
+var obj = {
+    num: 1
+};
+console.log(obj.d);
+console.log(typeof obj.d);
+// console.log(a); //error
+console.log(typeof a);
+if ("undefined" == typeof a) {
+    console.log("typeof a is undefined");
+}
+if ("undefined" == typeof obj.d) {
+    console.log("typeof obj.d is undefined");
+}
+console.log(typeof null);
